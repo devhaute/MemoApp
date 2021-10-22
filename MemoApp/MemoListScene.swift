6302db951004f8c3500d1f28fb9f6eed583fe124
devhaute <devhaute@gmail.com>
2021-10-22 19:01:47 +0900

    메모 리스트 구현

--- MemoApp/MemoListScene.swift
+++ MemoApp/MemoListScene.swift
@@ -1,21 +1,24 @@
-//
-//  ContentView.swift
-//  MemoApp
-//
-//  Created by devhaute on 2021/10/22.
-//
-
 import SwiftUI
 import CoreData
 
 struct MemoListScene: View {
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
-        Text("hello world!")
+        NavigationView {
+            List {
+                ForEach(store.list) { memo in
+                    Text(memo.content)
+                }
+            }
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 struct MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
         MemoListScene()
+            .environmentObject(MemoStore())
     }
 }
