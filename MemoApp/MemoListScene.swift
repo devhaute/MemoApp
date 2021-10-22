3ddd6c7580550bacd9ace5a97fda516e6a5adefc
devhaute <devhaute@gmail.com>
2021-10-22 17:49:35 +0900

    Project Setup

---
+++ MemoApp/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoApp
+//
+//  Created by devhaute on 2021/10/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("hello world!")
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
